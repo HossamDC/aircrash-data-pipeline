88674259e17f1b92df224c31c7cf0ed288153d76
Hossam Eldin <hossam772016@gmail.com>
2025-04-06 16:40:08 +0300

    Added new transformation spark script and updated README

--- scripts/spark-script.py
+++ scripts/spark-script.py
@@ -6,7 +6,7 @@ from pyspark.sql.functions import (
 # Create Spark Session
 spark = SparkSession.builder.appName("HFPlaneCrashes").getOrCreate()
 
-# Load CSV from S3 (with s3:// prefix in Spark)
+# Load CSV from S3
 df = spark.read.option("header", True).csv("s3://my-spark-stage-23-3-1998-v1-01/plane_crashes/raw_hf_airplane_crashes.csv")
 
 # Clean and cast numeric fields
@@ -19,34 +19,32 @@ df = df.withColumn("Crash_Date", to_date("Date", "MM/dd/yyyy")) \
        .withColumn("Year", year("Crash_Date")) \
        .withColumn("Month", month("Crash_Date"))
 
-# Aircraft Maker (first word in Type)
+# Aircraft Maker
 df = df.withColumn("Aircraft_Maker", trim(split(col("Type"), " ").getItem(0)))
 
 # Survivors
 df = df.withColumn("Survivors", (col("Aboard") - col("Fatalities")))
 
-# Crash Severity (% Fatalities / Aboard)
+# Crash Severity
 df = df.withColumn("Severity_Pct", round((col("Fatalities") / col("Aboard")) * 100, 1))
-
-# Categorize Crash Severity
 df = df.withColumn("Crash_Severity", when(col("Fatalities") == 0, "None")
                    .when(col("Severity_Pct") < 5, "Low")
                    .when(col("Severity_Pct") < 30, "Medium")
                    .otherwise("High"))
 
-# Flag
+# Is Fatal Flag
 df = df.withColumn("Is_Fatal", (col("Fatalities") > 0).cast("boolean"))
 
 # Final columns
 df_final = df.select(
-    "Crash_Date", "Year", "Month", "Location", "Operator", "Type",
+    "Crash_Date", "Year", "Location", "Operator", "Type",
     "Aircraft_Maker", "Aboard", "Fatalities", "Ground", "Survivors",
     "Is_Fatal", "Crash_Severity"
 )
 
-# Write as partitioned Parquet
+# Write Parquet partitioned by Year only
 df_final.write.mode("overwrite") \
-    .partitionBy("Year", "Month") \
+    .partitionBy("Year") \
     .parquet("s3://my-spark-stage-23-3-1998-v1-01/plane_crashes/processed_parquet/")
 
-print("✅ ETL Complete: Parquet written to S3.")
+print("✅ ETL Complete: Parquet written to S3 partitioned by Year only.")
